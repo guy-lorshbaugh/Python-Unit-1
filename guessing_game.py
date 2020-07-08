b8991eed8176b35b6f51b526b0aa33c0853b8e02
guy-lorshbaugh <67803858+guy-lorshbaugh@users.noreply.github.com>
2020-07-08 11:06:02 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -7,28 +7,31 @@ def start_game():
     high_score = 10
     while play_again.lower() == "y":
         number = random.randint(1,10)
-        guess = input("Guess a number between 1-10.  ")
         guess_count = 0
-        try:
-            guess = int(guess)
-        except ValueError:
-            print("Invalid entry.  Please enter a numeral value between 1-10.")
-        else:
-            while guess != number:
+
+        print(number)
+
+        guess = None
+
+        while guess != number:
+            guess = input("Guess a number between 1-10.  ")
+            try:  
+                guess = int(guess)
+            except ValueError:
+                print("Invalid entry.  Please enter a numeral value between 1-10.")
+            else:
                 if not (1 <= guess <= 10):
-                    guess = input("Your guess must be between 1-10. Please try again.  ")
+                    print("Your guess must be between 1-10. Please try again.  ")
                 else:
                     word = 'lower' if guess > number else 'higher'
-                    guess = input("It's {}!! Guess again:  ".format(word))
-                guess = int(guess)
-                guess_count += 1
-            print("You got it! It took you {} guesses.  Thanks for playing!!".format(guess_count))
-            if guess_count < high_score:
-                high_score = guess_count
-            play_again = input("\nWould you like to play again? Y/N  ").lower()
-            if play_again == "y":
-                print("\nThe high score so far is {}. Can you beat it?\n".format(high_score))
-            else:
-                print("\nThank you for playing! The game is now closing.\n")
-            
+                    print("It's {}!! Guess again:  ".format(word))
+                    guess_count += 1
+        print("You got it! It took you {} guesses.  Thanks for playing!!".format(guess_count))
+        if guess_count < high_score:
+            high_score = guess_count
+        play_again = input("\nWould you like to play again? Y/N  ").lower()
+        if play_again == "y":
+            print("\nThe high score so far is {}. Can you beat it?\n".format(high_score))
+        else:
+            print("\nThank you for playing! The game is now closing.\n")
 start_game()
